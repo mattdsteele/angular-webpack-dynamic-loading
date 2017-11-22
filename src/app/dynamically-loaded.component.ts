388757fd930e5353fa0a2e2ad380b0a4a06d3911
Matt Steele <orphum@gmail.com>
2017-11-22 14:49:13 -0600

    Add component (loaded statically)

---
+++ src/app/dynamically-loaded.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamically-loaded',
+  template: `<p>This component was loaded dynamically!</p>`
+})
+export class DynamicallyLoadedComponent {}
